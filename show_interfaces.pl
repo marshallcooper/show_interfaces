4af8644c9b999d68f3313969b4ed9bf214d493e6
marshallcooper <marshallcooper@gmail.com>
2017-05-16 10:08:48 -0700

    Create show_interfaces.pl

---
+++ show_interfaces.pl
@@ -0,0 +1,29 @@
+#!usr/bin/perl
+
+
+print "\nUsage: perl show_interfaces.pl <service type> <slot number> <port numbers seperated by a space>\n";
+
+print "\nshow int @ARGV[0] 1/@ARGV[1]/@ARGV[2]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[3]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[4]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[5]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[6]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[7]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[8]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[9]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[10]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[11]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[12]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[13]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[14]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[15]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[16]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[17]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[18]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[19]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[20]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[21]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[22]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[23]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[24]\n";
+print "show int @ARGV[0] 1/@ARGV[1]/@ARGV[25]\n";
